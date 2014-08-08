24e7deac42301c7c326e0ded9a6539d1d976a4e2
Gernot Vormayr <gvormayr@gmail.com>
2014-08-08 22:32:56 +0200

    assembler improvements

--- src/fft_mp.asm
+++ src/fft_mp.asm
@@ -1,3 +1,5 @@
+const N 256
+
 define R mem 0b10
 define I mem 0b11
 
@@ -11,25 +13,24 @@ define load(reg r1, reg i1, reg addr1, reg r2, reg i2) {
 
 define bfr(unsigned reg i, unsigned reg j, signed fix7 reg wr, signed fix7 reg wi) {
     tr = wr * R[j] - wi * I[j]
-    qr = (signed) R[i] >> 1
+    qr = signed R[i] >> 1
     R[j] = qr - tr
     R[i] = qr + tr
 }
 
 define bfi(unsigned last i, unsigned last j, signed fix7 last wr, signed fix7 last wi) {
     ti = wr * I[j] + wi * R[j]
-    qi = (signed) I[i] >> 1
+    qi = signed I[i] >> 1
     I[j] = qi - ti
     I[i] = qi + ti
 }
  
-
 ; sync
 
 BEGIN_SYNC:
-    CMPL SERIAL, 0x55
+    CMPL $SERIAL, 0x55
     JNE BEGIN_SYNC
-    CMPL SERIAL, 0xAA
+    CMPL $SERIAL, 0xAA
     JNE BEGIN_SYNC
 
     MOVL SERIAL, '1'
@@ -37,14 +38,14 @@ BEGIN_SYNC:
 LOAD:
     MOV $2, 0
 READ:
-    MOVL $0, SERIAL
-    MOVR $0, SERIAL     ; $0 = IR
-    MOVL $1, SERIAL
-    MOVR $1, SERIAL     ; $1 = IR
+    MOVL $0, $SERIAL
+    MOVR $0, $SERIAL     ; $0 = IR
+    MOVL $1, $SERIAL
+    MOVR $1, $SERIAL     ; $1 = IR
     load $0L, $0H, $2, $1L, $1H
     ADD $2, $2, 2
-    CMP $2, 256
-    JB READ
+    CMP $2, N
+    JULT READ
 
     MOV $0, 1           ; l
     MOV $1, 7           ; k
@@ -54,7 +55,7 @@ FFT_STEP:
 
 OUTER:
     SHL $4, $3, $1      ; j
-    ADD $5, $4, 64
+    ADD $5, $4, N/4
     MOVL $8, SINE[$4]        ; wi
     MOVL $7, SINE[$5]        ; wr
     SUB $8, 0, $8
@@ -65,28 +66,28 @@ INNER:
     bfr $5, $4, $7 $8
     bfi
     ADD $5, $5, $2
-    CMP $5, 256
-    JB INNER
+    CMP $5, N
+    JULT INNER
 
     ADD $3, $3, 1
     CMP $3, $0
-    JB OUTER
+    JULT OUTER
 
     SUB $1, $1, 1
     MOV $0, $2
-    CMP $0, 256
-    JB FFT_STEP
+    CMP $0, N
+    JULT FFT_STEP
 
 UNLOAD:
     MOV $0, 0
-    MOV SERIAL, R[$0]
-    MOV SERIAL, I[$0]
+    MOV $SERIAL, R[$0]
+    MOV $SERIAL, I[$0]
     ADD $0, $0, 1
-    CMP $0, 256
-    JB UNLOAD
+    CMP $0, N
+    JULT UNLOAD
     JMP LOAD
 
 SINE:
-for i in 0 to 255 {
+for i in 0 to N {
     DB sin(2 * M_PI * i / 256) * 128
 }
